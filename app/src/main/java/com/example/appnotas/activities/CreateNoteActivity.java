c34cee9b261744db217aa078947fe90debacc304
Jane <82067292+yh-jane@users.noreply.github.com>
2023-06-18 14:04:01 -0300

    update
    Corrigindo erro e atualizando

--- app/src/main/java/com/example/appnotas/activities/CreateNoteActivity.java
+++ app/src/main/java/com/example/appnotas/activities/CreateNoteActivity.java
@@ -2,6 +2,9 @@ package com.example.appnotas.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -10,6 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.appnotas.R;
+import com.example.appnotas.database.NotesDatabase;
 import com.example.appnotas.entities.Note;
 
 import java.text.SimpleDateFormat;
@@ -42,6 +46,14 @@ public class CreateNoteActivity extends AppCompatActivity {
                 new SimpleDateFormat ( "EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault())
                         .format(new Date())
         );
+
+        ImageView imageSave = findViewById(R.id.imageSave);
+        imageSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     private void saveNote(){
@@ -53,6 +65,27 @@ public class CreateNoteActivity extends AppCompatActivity {
             return;
         }
         final Note note = new Note();
-        note.setTitle();
+        note.setTitle(inputNoteTitle.getText().toString());
+        note.setSubtitle(inputNoteSubtitle.getText().toString());
+        note.setNoteText(inputNoteText.getText().toString());
+        note.setDateTime(textDateTime.getText().toString());
+
+        @SuppressLint("StaticFiledLeak")
+        class SaveNoteTask extends AsyncTask<Void, Void, Void>{
+
+            @Override
+            protected Void doInBackground(Void... voids){
+                NotesDatabase.getDatabase(getApplicationContext()).noteDao().insertNote(note);
+                return null;
+            }
+            @Override
+            protected void OnPostExecute (Void avoid){
+                super.onPostExecute(avoid);
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        }
+        new SaveNoteTask().execute();
     }
 }
