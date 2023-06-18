a9de962661e77ae48b84dd7274b05be78542590c
Jane <82067292+yh-jane@users.noreply.github.com>
2023-06-18 14:33:31 -0300

    Update CreateNoteActivity.java

--- app/src/main/java/com/example/appnotas/activities/CreateNoteActivity.java
+++ app/src/main/java/com/example/appnotas/activities/CreateNoteActivity.java
@@ -1,6 +1,9 @@
 package com.example.appnotas.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -19,11 +22,19 @@ import com.example.appnotas.entities.Note;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
+import androidx.lifecycle.ProcessLifecycleOwner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 public class CreateNoteActivity extends AppCompatActivity {
 
     private EditText inputNoteTitle, inputNoteSubtitle, inputNoteText;
     private TextView textDateTime;
+    private ExecutorService executor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +54,7 @@ public class CreateNoteActivity extends AppCompatActivity {
         textDateTime = findViewById(R.id.textDateTime);
 
         textDateTime.setText(
-                new SimpleDateFormat ( "EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault())
+                new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault())
                         .format(new Date())
         );
 
@@ -51,17 +62,20 @@ public class CreateNoteActivity extends AppCompatActivity {
         imageSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                saveNote();
             }
         });
+
+        executor = Executors.newSingleThreadExecutor();
+        ProcessLifecycleOwner.get().getLifecycle().addObserver(lifecycleObserver);
     }
 
-    private void saveNote(){
-        if (inputNoteTitle.getText().toString().trim().isEmpty()){
+    private void saveNote() {
+        if (inputNoteTitle.getText().toString().trim().isEmpty()) {
             Toast.makeText(this, "O Título da Nota não pode ser vazio", Toast.LENGTH_SHORT).show();
             return;
-        }else if (inputNoteSubtitle.getText().toString().trim().isEmpty()&& inputNoteText.getText().toString().trim().isEmpty()){
-            Toast.makeText(this, "Nota não pode ser vazio", Toast.LENGTH_SHORT).show();
+        } else if (inputNoteSubtitle.getText().toString().trim().isEmpty() && inputNoteText.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Nota não pode ser vazia", Toast.LENGTH_SHORT).show();
             return;
         }
         final Note note = new Note();
@@ -70,22 +84,20 @@ public class CreateNoteActivity extends AppCompatActivity {
         note.setNoteText(inputNoteText.getText().toString());
         note.setDateTime(textDateTime.getText().toString());
 
-        @SuppressLint("StaticFiledLeak")
-        class SaveNoteTask extends AsyncTask<Void, Void, Void>{
-
-            @Override
-            protected Void doInBackground(Void... voids){
-                NotesDatabase.getDatabase(getApplicationContext()).noteDao().insertNote(note);
-                return null;
-            }
-            @Override
-            protected void OnPostExecute (Void avoid){
-                super.onPostExecute(avoid);
+        executor.submit(() -> {
+            NotesDatabase.getDatabase(getApplicationContext()).noteDao().insertNote(note);
+            runOnUiThread(() -> {
                 Intent intent = new Intent();
                 setResult(RESULT_OK, intent);
                 finish();
-            }
-        }
-        new SaveNoteTask().execute();
+            });
+        });
     }
+
+    private final LifecycleObserver lifecycleObserver = new LifecycleObserver() {
+        @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+        public void onDestroy() {
+            executor.shutdown();
+        }
+    };
 }
